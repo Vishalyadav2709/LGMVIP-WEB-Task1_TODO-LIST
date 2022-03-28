a64ce948829cea68e3d78f7ad327880e59403fb2
vishalyadav <vishal.tyadav2709@gmail.com>
2022-03-28 09:39:01 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelector('#add').onclick = function(){
+    if(document.querySelector('#todo').value.length == 0){
+        alert("Please Enter a Task")
+    }
+
+    else{
+        document.querySelector('#viewlist').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#todo').value}
+                </span>
+                <button class="delete">
+                <img src="trash.png" alt="" >
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
